90a02648ae63b7030e7eb5f198b12dfb0a2a05a8
enocax <60621249+enocax@users.noreply.github.com>
2020-02-04 19:11:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+ from ota_update.main.ota_updater import OTAUpdater
+ from machine import Pin
+ from time import sleep 
+
+ def download_and_install_update_if_available():
+     o = OTAUpdater('https://github.com/enocax/ESP8266-Classe')
+     o.download_and_install_update_if_available('Raspberry', 'raspberry')
+
+
+ def start():
+     # your custom code goes here. Something like this: ...
+    p2= Pin(2, Pin.OUT)
+    while 1:
+        p2.on
+        sleep(2)
+        p2.off
+
+
+ def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+ boot()
+ 
